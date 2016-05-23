981e169d7fa9a76449cf3b99d83b09d6169a14b1
Sergii_Rud <Sergii_Rud@epam.com>
2016-05-23 20:09:46 +0300

    lab 3 done

---
+++ Lab 3/OwnHashFunc/OwnHashFunc/Hasher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace OwnHashFunc
+{
+    public static class Hasher
+    {
+        public static BitArray GetHashCode(byte[] sourse, int bitsResponse)
+        {
+            uint hashAddress = 0;
+            for (var i = 0; i < sourse.Length; i++)
+            {
+                hashAddress = sourse[i] + (hashAddress << 2) + (hashAddress << 7) - hashAddress;
+            }
+
+            var bitArray = new BitArray(bitsResponse);
+            for (int i = 0; i < bitsResponse; i++)
+            {
+                bitArray[i] = (hashAddress & (1 << i)) >> i == 1;
+            }
+
+            return bitArray;
+        }
+    }
+}
---
+++ Lab 3/OwnHashFunc/OwnHashFunc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace OwnHashFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordBytes = File.ReadAllBytes("word.docx");
+            var sourceBytes = File.ReadAllBytes("source.txt");
+            var imageBytes = File.ReadAllBytes("image.bmp");
+
+            var wordHash = Hasher.GetHashCode(wordBytes, 8);
+            var sourceHash = Hasher.GetHashCode(sourceBytes, 8);
+            var imageHash = Hasher.GetHashCode(imageBytes, 8);
+
+            var resultString = string.Format("Word: {1}{0}Source code: {2}{0}Image: {3}",
+                "\r\n",
+                GetStringHash(wordHash),
+                GetStringHash(sourceHash),
+                GetStringHash(imageHash));
+
+            File.WriteAllText("result_new.txt", resultString);
+        }
+
+        private static string GetStringHash(BitArray array)
+        {
+            return array.Cast<object>().Aggregate(string.Empty, (current, bit) => current + ((bool)bit ? "1" : "0"));
+        }
+    }
+}
